c05338aa6007db5a94c043f8bedfb96b9f15a9c9
Mohan Ratnam <mosesratnam@yahoo.com>
2023-10-03 11:55:48 +0100

    A C# Exercise 4.4 pre learning

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Exercise4._4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #nullable disable
+        int numWords; 
+        
+        Console.WriteLine("Welcome to Exercise 4.4 Pre learning!");
+
+        Console.WriteLine();
+        Console.Write("Enter number of words: ");
+        numWords = int.Parse(Console.ReadLine());
+        Console.WriteLine();   
+        
+        string[] wordArray = new string[numWords];
+        
+        for (int i = 0; i < numWords; i++)
+        {
+            Console.Write("Enter a word: ");
+            wordArray[i] = Console.ReadLine();
+        }
+
+        Console.Write("Your sentence is :");
+        for (int i = 0; i < numWords; i++)
+        {
+            Console.Write("{0} ", wordArray[i]);           
+        }
+        
+        Console.WriteLine();
+        Console.WriteLine("Your reversed words are :");
+        Array.Reverse(wordArray);
+
+        for (int i = 0; i < numWords; i++)
+        {
+            Console.WriteLine("{0} ", wordArray[i]);           
+        }
+        
+        //Console.WriteLine(wordArray.Length);
+    }
+}
